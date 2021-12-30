45b72f5c400690d256f16197e6663f6a7ad4e867
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2021-12-30 13:22:19 -0600

    conditional rendering

--- src/components/counterComponents.jsx
+++ src/components/counterComponents.jsx
@@ -3,40 +3,28 @@ import React, {Component} from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: []
 
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return   <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
+    }
 
 
     render() {
 
         return (
             <div>
-                <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-                </ul>
-            </div>
+                {this.state.tags.length === 0 && 'Please create new tag'}
+                {this.renderTags()} </div>
 
         );
 
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 ";
-        // the tutorial wants to put badge- in the above statement then just append
-        // warning and primary below.  I'm keeping this because I feel it is more readable
-        // for someone not familiar with the code.  Though this way I need a space either after m-2 or
-        //before badge-warning/primary
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-        return classes;
-    }
-
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
 }
 
 export default Counter;
