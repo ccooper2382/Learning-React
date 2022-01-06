ca5f3e7ce4937a4ad079239cd86a149d6d801670
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-01-06 15:45:38 -0600

    Add files via upload

--- counterComponents.jsx
+++ counterComponents.jsx
@@ -12,16 +12,19 @@ class Counter extends Component {
     //    this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = () => {
-        console.log('clicked', this);
-    }
+    handleIncrement = product => {
+        console.log(product);
+       this.setState({count: this.state.count +1})  }
+
+
 
     render() {
 
         return (
             <div>
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement("rawr") }
+                        className="btn btn-secondary btn-sm">Increment</button>
             </div>
 
         );
