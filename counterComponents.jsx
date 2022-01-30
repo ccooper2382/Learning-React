bf5898aea8377bf9fb3f14c161d1100b4f8c928b
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-01-30 11:45:30 -0600

    Working with the 'children' prop
    https://codewithmosh.com/courses/357787/lectures/5634710

--- counterComponents.jsx
+++ counterComponents.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.value
 
     };
     //This is one way to bind eventhandlers to this the arrow function below is another but is experimental
@@ -13,17 +13,19 @@ class Counter extends Component {
     // }
 
     handleIncrement = product => {
-        console.log(product);
-       this.setState({count: this.state.count +1})  }
+
+       this.setState({value: this.state.value +1})  }
 
 
 
     render() {
+        console.log(this.props);
 
         return (
             <div>
+                {this.props.children}
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement("rawr") }
+                <button onClick={() => this.handleIncrement() }
                         className="btn btn-secondary btn-sm">Increment</button>
             </div>
 
@@ -37,13 +39,13 @@ class Counter extends Component {
         // warning and primary below.  I'm keeping this because I feel it is more readable
         // for someone not familiar with the code.  Though this way I need a space either after m-2 or
         //before badge-warning/primary
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
 
