16008b6971fa810c3e3daf332cc0fa202d968916
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-02-03 13:28:28 -0600

    Add files via upload

--- counter.jsx
+++ counter.jsx
@@ -1,21 +1,13 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
 
-    };
     //This is one way to bind eventhandlers to this the arrow function below is another but is experimental
     //it seems to be working as of 12/30/21
     // constructor() {
     //     super();
     //    this.handleIncrement = this.handleIncrement.bind(this);
     // }
-
-    handleIncrement = () => {
-
-       this.setState({value: this.state.value +1})  }
-
 
 
     render() {
@@ -25,7 +17,7 @@ class Counter extends Component {
             <div>
 
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement() }
+                <button onClick={() => this.props.onIncrement(this.props.counter) }
                         className="btn btn-secondary btn-sm">Increment</button>
                 <button onClick={()=> this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
@@ -40,12 +32,12 @@ class Counter extends Component {
         // warning and primary below.  I'm keeping this because I feel it is more readable
         // for someone not familiar with the code.  Though this way I need a space either after m-2 or
         //before badge-warning/primary
-        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
+        classes += this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
     formatCount() {
-        const {value} = this.state;
+        const {value} = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
