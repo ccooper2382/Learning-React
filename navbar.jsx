355f5ce51e75f0049e58720de1991f14b6605f35
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-02-04 14:31:44 -0600

    Add files via upload

--- navbar.jsx
+++ navbar.jsx
@@ -6,6 +6,7 @@ class Navbar extends Component {
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
+                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
                 </div>
             </nav>
         );
