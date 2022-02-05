467f62c8b4f7a0092897ecceb02d13e988622c7d
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-02-05 14:24:55 -0600

    Add files via upload

--- navbar.jsx
+++ navbar.jsx
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
 
-class Navbar extends Component {
-    render() {
+
+//stateless functional component
+//using argument destructuring instead of passing props to the Navbar function use parenthesis and curly brackets to
+//pass only the methods needed. changing the prop.totalCounters to just totalCounters
+const Navbar = ({totalCounters}) => {
         return (
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                    <a className="navbar-brand" href="#">Navbar{" "}</a>
+                    <span className="badge badge-pill badge-secondary">{totalCounters}</span>
                 </div>
             </nav>
         );
     }
-}
+
 
 export default Navbar;
